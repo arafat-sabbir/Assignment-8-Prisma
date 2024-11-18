be74e6e1692715d25a9ca372d9cc8b637eeb557f
arafat-sabbir <arafatshabbir8@gmail.com>
2024-11-18 12:17:12 +0600

    add controller function to get all and single book

--- src/app/modules/book/book.controller.ts
+++ src/app/modules/book/book.controller.ts
@@ -3,6 +3,7 @@ import catchAsync from "../../utils/catchAsync";
 import { bookServices } from "./book.service";
 import sendResponse from "../../utils/sendResponse";
 
+// Controller Function to Add A New Book
 const addNewBook = catchAsync(async (req: Request, res: Response) => {
   const result = await bookServices.addNewBookIntoDb(req.body);
   sendResponse(res, {
@@ -11,4 +12,22 @@ const addNewBook = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-export const bookControllers = { addNewBook };
+//Controller Function to Get All The Books
+const getAllBook = catchAsync(async (req: Request, res: Response) => {
+  const result = await bookServices.getAllBookFromDb();
+  sendResponse(res, {
+    message: "All Books retrieved successfully",
+    data: result,
+  });
+});
+
+// Controller Function To Get Single Book
+const getSingleBook = catchAsync(async (req: Request, res: Response) => {
+  const result = await bookServices.getSingleBookFromDb(req.params.bookId);
+  sendResponse(res, {
+    message: "Single Book retrieved successfully",
+    data: result,
+  });
+});
+
+export const bookControllers = { addNewBook, getAllBook, getSingleBook };
--- src/app/modules/book/book.route.ts
+++ src/app/modules/book/book.route.ts
@@ -3,6 +3,9 @@ import { bookControllers } from "./book.controller";
 
 const router = express.Router();
 
+// Router To Create A New Book
 router.post("/", bookControllers.addNewBook);
+
+// router.get("/", bookControllers.);
 
 export const bookRoutes = router;
