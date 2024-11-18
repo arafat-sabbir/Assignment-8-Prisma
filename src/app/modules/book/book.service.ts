2487ce1fa74e33306f837fcb37721db9e497a4cf
arafat-sabbir <arafatshabbir8@gmail.com>
2024-11-18 11:56:17 +0600

    create a catchAsync function To Resolve Async Function Error

--- src/app/modules/book/book.service.ts
+++ src/app/modules/book/book.service.ts
@@ -1,6 +1,7 @@
 import { Book } from "@prisma/client";
 import { prisma } from "../../../app";
 
+
 // Service Function To Add A New Book Into Database
 const addNewBookIntoDb = async (data: Book) => {
   const result = await prisma.book.create({
@@ -9,10 +10,14 @@ const addNewBookIntoDb = async (data: Book) => {
   return result;
 };
 
+
+// Service Function To Retrieve All The Books From Database
 const getAllBookFromDb = async () => {
   return await prisma.book.findMany();
 };
 
+
+// Service Function To Retrieve Specific  Book By It's Id From Database
 const getSingleBookFromDb = async (bookId: string) => {
   return await prisma.book.findUnique({
     where: { bookId },
