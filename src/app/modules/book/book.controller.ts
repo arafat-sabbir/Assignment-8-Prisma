f032356b03bba511ef17771d0f9f2d463e36ed72
arafat-sabbir <arafatshabbir8@gmail.com>
2024-11-18 12:02:10 +0600

    create controller function to handle Add New Book

--- src/app/modules/book/book.controller.ts
+++ src/app/modules/book/book.controller.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
 import catchAsync from "../../utils/catchAsync";
 import { bookServices } from "./book.service";
+import sendResponse from "../../utils/sendResponse";
 
 const addNewBook = catchAsync(async (req: Request, res: Response) => {
   const result = await bookServices.addNewBookIntoDb(req.body);
-  res.send(result);
+  sendResponse(res, {
+    message: "New Book added successfully",
+    data: result,
+  });
 });
 
 export const bookControllers = { addNewBook };
